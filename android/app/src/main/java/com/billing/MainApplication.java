1a03b2104a5e8ea4d33bc6e17f6d44eadf59552b
vigneshpy <vickypotter2516@gmail.com>
2021-07-25 22:32:35 +0530

    fix:added env file

--- android/app/src/main/java/com/billing/MainApplication.java
+++ android/app/src/main/java/com/billing/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
